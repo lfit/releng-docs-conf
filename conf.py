be1f9af9592f391f0b7927c7ec71f4427e0ed025
Thanh Ha <thanh.ha@linuxfoundation.org>
2017-11-08 16:01:30 -0500

    Create a initial conf.py based on releng/docs
    Change-Id: Ie94adef66297e512b19d93e3f84229577ab77abc
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

---
+++ conf.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Common conf.py configuration for lf-releng projects."""
+
+import os
+import sys
+import sphinx_bootstrap_theme
+import yaml
+sys.path.insert(0, os.path.abspath('.'))
+
+with open('../INFO.yaml', 'r') as yaml_file:
+    info = yaml.load(yaml_file)
+
+# -- General configuration ------------------------------------------------
+needs_sphinx = '1.6'
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+    'sphinxcontrib.programoutput',
+]
+
+master_doc = 'index'
+source_suffix = ['.rst']
+templates_path = ['_templates']
+
+# General information about the project.
+project = info['project-name']
+author = info['author']
+copyright = info['copyright']
+
+# The short X.Y version.
+version = info['version']
+# The full version, including alpha/beta/rc tags.
+release = version
+
+language = None
+exclude_patterns = []
+pygments_style = 'sphinx'
+todo_include_todos = True
+intersphinx_mapping = {
+    'global-jjb': ('http://global-jjb.releng.linuxfoundation.org/en/latest/', None),
+    'lfdocs': ('http://docs.releng.linuxfoundation.org/en/latest/', None),
+    'lftools': ('http://lftools.releng.linuxfoundation.org/en/latest/', None),
+    'python': ('https://docs.python.org/', None),
+}
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = 'bootstrap'
+html_theme_options = {
+    'bootswatch_theme': "cerulean",
+    'navbar_sidebarrel': False,
+    'source_link_position': "footer",
+}
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
+html_logo = '_static/lf-logo-small.png'
+html_favicon = 'favicon.ico'
+html_static_path = ['_static']
+html_sidebars = {
+    '**': ['localtoc.html', 'relations.html'],
+}
+
+htmlhelp_basename = 'LFRelengDoc'
+
+# If confext.py exists in the same directory as conf.py this is a config
+# override file allowing a project to override any configuration in this file.
+try:
+    print('confext.py found, overriding config.')
+    from confext import *
+except ImportError:
+    pass
