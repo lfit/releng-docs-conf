63b2507c50b718446a3a02c5854336a07e583140
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-01-24 20:13:36 -0500

    Add sys path for rtd
    Change-Id: I820fc0e5d183088a1128ab84f2d11fd3b32a0268
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,11 @@
 # http://www.eclipse.org/legal/epl-v10.html
 ##############################################################################
 
+import os
 import pkg_resources
+import sys
+# Sys.path for RTD to resolve docs_conf package
+sys.path.insert(0, os.path.abspath('..'))
 
 from docs_conf import *
 
