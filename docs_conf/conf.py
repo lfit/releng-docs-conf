36a5aca8588a19ba47e3a7e1b1590d36e1f16935
Thanh Ha <thanh.ha@linuxfoundation.org>
2018-06-15 15:55:28 -0400

    Update lfdocs links to https
    Save us on redirects when fetching the links.
    
    Change-Id: Ic0d5290159a4af5bea54444ba4f106cc28ce3480
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- docs_conf/conf.py
+++ docs_conf/conf.py
@@ -107,10 +107,10 @@ else:
 htmlhelp_basename = cfg.get('htmlhelp_basename', 'DocsConf')
 
 intersphinx_mapping = {
-    'common-packer': ('http://docs.releng.linuxfoundation.org/projects/common-packer/en/latest/', None),
-    'global-jjb': ('http://docs.releng.linuxfoundation.org/projects/global-jjb/en/latest/', None),
-    'lfdocs': ('http://docs.releng.linuxfoundation.org/en/latest/', None),
-    'lfdocs-conf': ('http://docs.releng.linuxfoundation.org/projects/lfdocs-conf/en/latest/', None),
-    'lftools': ('http://docs.releng.linuxfoundation.org/projects/lftools/en/latest/', None),
+    'common-packer': ('https://docs.releng.linuxfoundation.org/projects/common-packer/en/latest/', None),
+    'global-jjb': ('https://docs.releng.linuxfoundation.org/projects/global-jjb/en/latest/', None),
+    'lfdocs': ('https://docs.releng.linuxfoundation.org/en/latest/', None),
+    'lfdocs-conf': ('https://docs.releng.linuxfoundation.org/projects/lfdocs-conf/en/latest/', None),
+    'lftools': ('https://docs.releng.linuxfoundation.org/projects/lftools/en/latest/', None),
     'python': ('https://docs.python.org/', None),
 }
